e7703f461e512377e849db753ae088d9f9cef365
ILW8 <daohengliu@gmail.com>
2021-10-28 22:30:05 +0100

    login: add global auth checker

--- src/components/GlobalLayout.js
+++ src/components/GlobalLayout.js
@@ -22,13 +22,19 @@ class GlobalLayout extends React.Component {
     };
   }
 
-  // componentDidMount() {
-  //   fetch('/api/settings/account_settings/')
-  //     .then((data) => data.json())
-  //     .then((p) => {
-  //       console.log(p);
-  //     });
-  // }
+  componentDidMount() {
+    fetch('/api/settings/account_settings/')
+      .then((resp) => {
+        if (resp.status === 200) {
+          console.log(resp.status);
+          resp.json().then((data) => {
+            const { authState } = this.state;
+            authState.auth = data.user;
+            this.setState({ authState });
+          }).catch((e) => { console.log(`failed parsing json ${e}`); });
+        }
+      });
+  }
 
   render() {
     const { authState } = this.state;
